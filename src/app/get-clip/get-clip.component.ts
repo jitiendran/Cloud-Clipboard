adacf0763ffd14d55514c44677fd42a193f0868a
jitiendran <jitiendranjiji2000@gmail.com>
2022-01-08 16:55:29 +0530

    UI Changes : Get and Set Clip

--- src/app/get-clip/get-clip.component.ts
+++ src/app/get-clip/get-clip.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-get-clip',
   templateUrl: './get-clip.component.html',
-  styleUrls: ['./get-clip.component.scss']
+  styleUrls: ['./get-clip.component.scss'],
 })
 export class GetClipComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  year?: number;
+  constructor() {
+    this.year = new Date().getFullYear();
   }
 
+  ngOnInit(): void {}
 }
--- src/app/set-clip/set-clip.component.ts
+++ src/app/set-clip/set-clip.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-set-clip',
   templateUrl: './set-clip.component.html',
-  styleUrls: ['./set-clip.component.scss']
+  styleUrls: ['./set-clip.component.scss'],
 })
 export class SetClipComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  year?: number;
+  constructor() {
+    this.year = new Date().getFullYear();
   }
-
+  ngOnInit(): void {}
 }
