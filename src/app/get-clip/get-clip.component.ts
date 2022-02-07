d723a58c960d1aafddbea2142a466b3364b6a63b
jitiendran <jitiendranjiji2000@gmail.com>
2022-02-07 17:03:25 +0530

    Integration : Both Get and Set Clipboard

--- src/app/get-clip/get-clip.component.ts
+++ src/app/get-clip/get-clip.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { API } from 'src/environments/environment';
 
 @Component({
   selector: 'app-get-clip',
@@ -8,10 +11,31 @@ import { Component, OnInit } from '@angular/core';
 export class GetClipComponent implements OnInit {
   year?: number;
   data: string;
-  constructor() {
+  text: FormGroup = new FormGroup({});
+  url: string;
+  loading: boolean;
+
+  
+  constructor(private http:HttpClient) {
     this.year = new Date().getFullYear();
-    this.data = 'Jitiendran';
+    this.data = '';
+    this.url = `${API}/getClip`;
+    this.loading = false;
+    this.text = new FormGroup({
+        cid: new FormControl(''),
+        password: new FormControl(''),
+    });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
+
+  onGetClip() {
+    this.loading = true;
+    this.http.post<any>(this.url,this.text.value)
+    .subscribe(data => {
+        this.data = data.content;
+        this.loading = false;
+    });
+  }
 }
